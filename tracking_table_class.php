531ca027f77614198a01dea4818e2a33f5e01ec5
Saleem <sal4sup@gmail.com>
2022-01-01 02:29:09 +0200

    add settings to the plugin

---
+++ tracking_table_class.php
@@ -0,0 +1,206 @@
+<?php
+// WP_List_Table is not loaded automatically so we need to load it in our application
+if( ! class_exists( 'WP_List_Table' ) ) {
+    require_once( ABSPATH . 'wp-admin/includes/class-wp-list-table.php' );
+}
+/**
+ * Create a new table class that will extend the WP_List_Table
+ */
+class tracking_status_List_Table extends WP_List_Table
+{
+    /**
+     * Prepare the items for the table to process
+     *
+     * @return Void
+     */
+    public function prepare_items()
+    {
+        $columns = $this->get_columns();
+        $hidden = $this->get_hidden_columns();
+        $sortable = $this->get_sortable_columns();
+
+        $data = $this->table_data();
+        usort( $data, array( &$this, 'sort_data' ) );
+
+        $perPage = 10;
+        $currentPage = $this->get_pagenum();
+        $totalItems = count($data);
+
+        $this->set_pagination_args( array(
+            'total_items' => $totalItems,
+            'per_page'    => $perPage
+        ) );
+
+        $data = array_slice($data,(($currentPage-1)*$perPage),$perPage);
+
+        $this->_column_headers = array($columns, $hidden, $sortable);
+        $this->items = $data;
+    }
+
+    /**
+     * Override the parent columns method. Defines the columns to use in your listing table
+     *
+     * @return Array
+     */
+    public function get_columns()
+    {
+        $order_status1=get_option('order_status1');
+        $order_status2=get_option('order_status2');
+        $order_status3=get_option('order_status3');
+
+        $columns = array(
+            'id'          => 'ID',
+            'order_status1'       => substr($order_status1,3).' Date',
+            'order_status2' => substr($order_status2,3).' Date',
+            'order_status3'        => substr($order_status3,3).' Date',
+            'order_status1to2'    => substr($order_status1,3).' to '.substr($order_status2,3),
+            'order_status1to3'      => substr($order_status1,3).' to '.substr($order_status3,3)
+        );
+
+        return $columns;
+    }
+
+    /**
+     * Define which columns are hidden
+     *
+     * @return Array
+     */
+    public function get_hidden_columns()
+    {
+        return array();
+    }
+
+    /**
+     * Define the sortable columns
+     *
+     * @return Array
+     */
+    public function get_sortable_columns()
+    {
+        return array('title' => array('title', false));
+    }
+
+    /**
+     * Get the table data
+     *
+     * @return Array
+     */
+    private function table_data()
+    {
+        $order_status1=get_option('order_status1');
+        $order_status2=get_option('order_status2');
+        $order_status3=get_option('order_status3');
+        $order_status_calculate=get_option('order_status_calculate');
+
+        $orders = wc_get_orders( array(
+            'limit'        => -1, // Query all orders
+            'orderby'      => 'date',
+            'order'        => 'DESC',
+            'meta_key'     => 'order_status1', // The postmeta key field
+            'meta_value'     => null, // The postmeta key value
+            'meta_compare' => '!=', // The comparison argument
+        ));
+        $data = array();
+
+        foreach ($orders as $order){
+            $order_status1_meta=get_post_meta($order->get_id(),'order_status1',true);
+            $order_status2_meta=get_post_meta($order->get_id(),'order_status2',true);
+            $order_status3_meta=get_post_meta($order->get_id(),'order_status3',true);
+
+            $order_status1_date = new DateTime($order_status1_meta);
+
+            if(!empty($order_status2_meta)){
+                $order_status2_date = new DateTime($order_status2_meta);
+                $numberDays1to2 = $order_status2_date->diff($order_status1_date);
+                $numberDays1to2d=$numberDays1to2->format('%a Day and %h hours and %i minutes');
+
+            }
+            else{
+                $numberDays1to2d="";
+            }
+            if(!empty($order_status3_meta)){
+                $order_status3_date = new DateTime($order_status3_meta);
+                $numberDays2to3 = $order_status3_date->diff($order_status1_date);
+                $numberDays2to3d =$numberDays2to3->format('%a Day and %h hours and %i minutes');
+            }
+            else{
+                $numberDays2to3d="";
+            }
+
+            $data[] = array(
+                'id'          => $order->get_id(),
+                'order_status1'       => $order_status1_meta,
+                'order_status2' => $order_status2_meta,
+                'order_status3'        => $order_status3_meta,
+                'order_status1to2'    => $numberDays1to2d,
+                'order_status1to3'      => $numberDays2to3d
+            );
+
+
+        }
+
+
+
+
+
+        return $data;
+    }
+
+    /**
+     * Define what data to show on each column of the table
+     *
+     * @param  Array $item        Data
+     * @param  String $column_name - Current column name
+     *
+     * @return Mixed
+     */
+    public function column_default( $item, $column_name )
+    {
+        switch( $column_name ) {
+            case 'id':
+            case 'order_status1':
+            case 'order_status2':
+            case 'order_status3':
+            case 'order_status1to2':
+            case 'order_status1to3':
+                return $item[ $column_name ];
+
+            default:
+                return print_r( $item, true ) ;
+        }
+    }
+
+    /**
+     * Allows you to sort the data by the variables set in the $_GET
+     *
+     * @return Mixed
+     */
+    private function sort_data( $a, $b )
+    {
+        // Set defaults
+        $orderby = 'order_status1';
+        $order = 'asc';
+
+        // If orderby is set, use this as the sort column
+        if(!empty($_GET['orderby']))
+        {
+            $orderby = $_GET['orderby'];
+        }
+
+        // If order is set use this as the order
+        if(!empty($_GET['order']))
+        {
+            $order = $_GET['order'];
+        }
+
+
+        $result = strcmp( $a[$orderby], $b[$orderby] );
+
+        if($order === 'asc')
+        {
+            return $result;
+        }
+
+        return -$result;
+    }
+}
--- wc-tracking-status.php
+++ wc-tracking-status.php
@@ -13,6 +13,7 @@ Author URI: https://lvendr.com/
 */
 function order_status_date_menu() {
     add_menu_page( 'Order status', 'Order status', 'manage_options', 'order-status-id', 'order_status_date_options','',6 );
+    add_submenu_page( 'order-status-id', 'Order status Setting', 'Order status Setting', 'manage_options', 'order_status_settings','order_status_setting_display');
 
 }
 add_action( 'admin_menu', 'order_status_date_menu');
@@ -29,17 +30,148 @@ function order_status_date_options(){
 function order_status_date_display(){
     ?>
     <?php
-    $exampleListTable = new Example_List_Table();
-    $exampleListTable->prepare_items();
+    $tracking_table = new tracking_status_List_Table();
+    $tracking_table->prepare_items();
     ?>
     <div class="wrap">
         <div id="icon-users" class="icon32"></div>
-        <h2>Woocommerce tracking order status date</h2>
-        <?php $exampleListTable->display(); ?>
+        <h2>Orders delivery time tracking</h2>
+        <?php $tracking_table->display(); ?>
     </div>
     <?php
 
 }
+
+/*
+ * date for order status settings
+ */
+function order_status_setting_display(){
+    $order_status1=get_option('order_status1');
+    $order_status2=get_option('order_status2');
+    $order_status3=get_option('order_status3');
+    $order_status_calculate=get_option('order_status_calculate');
+
+    if(isset($_POST['submit'])){
+        if(isset($_POST['order_status1'])){
+            update_option('order_status1',$_POST['order_status1'],'');
+        }
+        if(isset($_POST['order_status2'])){
+            update_option('order_status2',$_POST['order_status2'],'');
+        }
+        if(isset($_POST['order_status3'])){
+            update_option('order_status3',$_POST['order_status3'],'');
+        }
+        if(isset($_POST['order_status_calculate'])){
+            update_option('order_status_calculate',$_POST['order_status_calculate'],'');
+        }
+
+    }
+    ?>
+    <div class="wrap">
+        <div id="icon-users" class="icon32"></div>
+        <h2>Orders status time tracking</h2>
+        <form method="post"  novalidate="novalidate">
+            <table class="form-table" role="presentation">
+
+                <tbody>
+                <tr>
+                    <th scope="row"><label for="order_status1">Order Status1</label></th>
+                    <td>
+                        <select name="order_status1" id="order_status1">
+                            <option></option>
+                            <?php
+                            foreach (wc_get_order_statuses() as $status_key=>$status_value){
+                                echo '<option value="'.$status_key.'" '.($status_key==$order_status1 ? 'selected' : '').'>'.$status_value.'</option>';
+                            }
+
+                            ?>
+                        </select>
+                    </td>
+
+                </tr>
+                <tr>
+                    <th scope="row"><label for="order_status2">Order Status2</label></th>
+                    <td>
+                        <select name="order_status2" id="order_status2">
+                            <option></option>
+                            <?php
+                            foreach (wc_get_order_statuses() as $status_key=>$status_value){
+                                echo '<option value="'.$status_key.'" '.($status_key==$order_status2 ? 'selected' : '').'>'.$status_value.'</option>';
+
+                            }
+
+                            ?>
+                        </select>
+                    </td>
+
+                </tr>
+                <tr>
+                    <th scope="row"><label for="order_status3">Order Status3</label></th>
+                    <td>
+                        <select name="order_status3" id="order_status3">
+                            <option></option>
+                            <?php
+                            foreach (wc_get_order_statuses() as $status_key=>$status_value){
+                                echo '<option value="'.$status_key.'" '.($status_key==$order_status3 ? 'selected' : '').'>'.$status_value.'</option>';
+                            }
+
+                            ?>
+                        </select>
+                    </td>
+
+                </tr>
+                <tr>
+                    <th scope="row">Calculate by</th>
+                    <td>
+                        <fieldset>
+                            <label for="order_status_calculate_days">
+                                <input name="order_status_calculate[]" <?php echo (in_array('days',$order_status_calculate) ? ' checked="checked"' : '');?> type="checkbox" id="order_status_calculate_days" value="days">
+                                Days
+                            </label>
+                            <label for="order_status_calculate_hours">
+                                <input name="order_status_calculate[]" <?php echo (in_array('hours',$order_status_calculate) ? ' checked="checked"' : '');?>  type="checkbox" id="order_status_calculate_hours" value="hours">
+                                Hours
+                            </label>
+                            <label for="order_status_calculate_minutes">
+                                <input name="order_status_calculate[]" <?php echo (in_array('minutes',$order_status_calculate) ? ' checked="checked"' : '');?>  type="checkbox" id="order_status_calculate_minutes" value="minutes">
+                                Minutes
+                            </label>
+                        </fieldset>
+                    </td>
+                </tr>
+                </tbody>
+            </table>
+
+
+            <p class="submit">
+                <input type="submit" name="submit" id="submit" class="button button-primary" value="Save Changes">
+            </p>
+        </form>
+    </div>
+
+    <?php
+}
+
+/*
+ * Register Options for the first time
+ */
+function register_status_setting_options() {
+
+    if ( !get_option('order_status1') ){
+        add_option('order_status1','processing','','yes');
+    }
+    if ( !get_option('order_status2') ){
+        add_option('order_status2','dispatch','','yes');
+    }
+    if ( !get_option('order_status3') ){
+        add_option('order_status3','completed','','yes');
+    }
+    if ( !get_option('order_status_calculate') ){
+        add_option('order_status_calculate','days','','yes');
+    }
+
+}
+//add_action( 'init', 'register_status_setting_options' );
 
 /*
  * update the date for order status
@@ -47,16 +179,22 @@ function order_status_date_display(){
 add_action( 'woocommerce_thankyou', 'update_order_processing_date', 20 );
 function update_order_processing_date ( $order_id )
 {
+    $order_status1=get_option('order_status1');
+    $order_status2=get_option('order_status2');
+    $order_status3=get_option('order_status3');
+    $order_status_calculate=get_option('order_status_calculate');
+
     $order = wc_get_order($order_id);
     $order_status = $order->get_status();
-    if ( $order_status == 'processing' && empty(get_post_meta($order_id,'processing_date',true))) {
-        update_post_meta($order_id,'processing_date',current_time( 'Y-m-d H:i:s' ));
+
+    if ( $order_status == substr($order_status1,3) && empty(get_post_meta($order_id,'order_status1',true))) {
+        update_post_meta($order_id,'order_status1',current_time( 'Y-m-d H:i:s' ));
     }
-    if($order_status=='dispatch-ready' && empty(get_post_meta($order_id,'dispatch_date',true))){
-        update_post_meta($order_id,'dispatch_date',current_time( 'Y-m-d H:i:s' ));
+    if($order_status== substr($order_status1,3) && empty(get_post_meta($order_id,'order_status2',true))){
+        update_post_meta($order_id,'order_status2',current_time( 'Y-m-d H:i:s' ));
     }
-    if($order_status=='completed' && empty(get_post_meta($order_id,'completed_date',true))){
-        update_post_meta($order_id,'completed_date',current_time( 'Y-m-d H:i:s' ));
+    if($order_status== substr($order_status1,3) && empty(get_post_meta($order_id,'order_status3',true))){
+        update_post_meta($order_id,'order_status3',current_time( 'Y-m-d H:i:s' ));
     }
 }
 /*
@@ -64,6 +202,11 @@ function update_order_processing_date ( $order_id )
      */
 add_action( 'save_post', 'update_order_status_date_dashboard', 10, 3 );
 function update_order_status_date_dashboard( $post_id, $post, $update ){
+    $order_status1=get_option('order_status1');
+    $order_status2=get_option('order_status2');
+    $order_status3=get_option('order_status3');
+    $order_status_calculate=get_option('order_status_calculate');
+
 
     // Orders in backend only
     if( ! is_admin() ) return;
@@ -74,211 +217,15 @@ function update_order_status_date_dashboard( $post_id, $post, $update ){
     $order = wc_get_order( $post_id );
     $order_status  = $order->get_status();
 
-    if ( $order_status == 'processing' && empty(get_post_meta($post_id,'processing_date',true))) {
-        update_post_meta($post_id,'processing_date',current_time( 'Y-m-d H:i:s' ));
+    if ( $order_status == substr($order_status1,3) && empty(get_post_meta($post_id,'order_status1',true))) {
+        update_post_meta($post_id,'order_status1',current_time( 'Y-m-d H:i:s' ));
     }
-    if($order_status=='dispatch-ready' && empty(get_post_meta($post_id,'dispatch_date',true))){
-        update_post_meta($post_id,'dispatch_date',current_time( 'Y-m-d H:i:s' ));
+    if($order_status==substr($order_status2,3) && empty(get_post_meta($post_id,'order_status2',true))){
+        update_post_meta($post_id,'order_status2',current_time( 'Y-m-d H:i:s' ));
     }
-    if($order_status=='completed' && empty(get_post_meta($post_id,'completed_date',true))){
-        update_post_meta($post_id,'completed_date',current_time( 'Y-m-d H:i:s' ));
+    if($order_status==substr($order_status3,3) && empty(get_post_meta($post_id,'order_status3',true))){
+        update_post_meta($post_id,'order_status3',current_time( 'Y-m-d H:i:s' ));
     }
 
 }
 
-// WP_List_Table is not loaded automatically so we need to load it in our application
-if( ! class_exists( 'WP_List_Table' ) ) {
-    require_once( ABSPATH . 'wp-admin/includes/class-wp-list-table.php' );
-}
-/**
- * Create a new table class that will extend the WP_List_Table
- */
-class Example_List_Table extends WP_List_Table
-{
-    /**
-     * Prepare the items for the table to process
-     *
-     * @return Void
-     */
-    public function prepare_items()
-    {
-        $columns = $this->get_columns();
-        $hidden = $this->get_hidden_columns();
-        $sortable = $this->get_sortable_columns();
-
-        $data = $this->table_data();
-        usort( $data, array( &$this, 'sort_data' ) );
-
-        $perPage = 10;
-        $currentPage = $this->get_pagenum();
-        $totalItems = count($data);
-
-        $this->set_pagination_args( array(
-            'total_items' => $totalItems,
-            'per_page'    => $perPage
-        ) );
-
-        $data = array_slice($data,(($currentPage-1)*$perPage),$perPage);
-
-        $this->_column_headers = array($columns, $hidden, $sortable);
-        $this->items = $data;
-    }
-
-    /**
-     * Override the parent columns method. Defines the columns to use in your listing table
-     *
-     * @return Array
-     */
-    public function get_columns()
-    {
-        $columns = array(
-            'id'          => 'ID',
-            'processing_date'       => 'Processing Date',
-            'dispatch_date' => 'Dispatch Date',
-            'completed_date'        => 'Completed Date',
-            'processing_dispatch'    => 'Processing to Dispatch',
-            'processing_completed'      => 'Processing to Completed'
-        );
-
-        return $columns;
-    }
-
-    /**
-     * Define which columns are hidden
-     *
-     * @return Array
-     */
-    public function get_hidden_columns()
-    {
-        return array();
-    }
-
-    /**
-     * Define the sortable columns
-     *
-     * @return Array
-     */
-    public function get_sortable_columns()
-    {
-        return array('title' => array('title', false));
-    }
-
-    /**
-     * Get the table data
-     *
-     * @return Array
-     */
-    private function table_data()
-    {
-        $orders = wc_get_orders( array(
-            'limit'        => -1, // Query all orders
-            'orderby'      => 'date',
-            'order'        => 'DESC',
-            'meta_key'     => 'processing_date', // The postmeta key field
-            'meta_value'     => null, // The postmeta key value
-            'meta_compare' => '!=', // The comparison argument
-        ));
-        $data = array();
-
-        foreach ($orders as $order){
-            $processing_date=get_post_meta($order->get_id(),'processing_date',true);
-            $dispatch_date=get_post_meta($order->get_id(),'dispatch_date',true);
-            $completed_date=get_post_meta($order->get_id(),'completed_date',true);
-
-            $processing_date1 = new DateTime($processing_date);
-
-            if(!empty($dispatch_date)){
-                $dispatch_date1 = new DateTime($dispatch_date);
-                $numberDays_dispatch = $dispatch_date1->diff($processing_date1);
-                $numberDays_dispatch1=$numberDays_dispatch->format('%a Day and %h hours and %i minutes');
-
-            }
-            else{
-                $numberDays_dispatch1="";
-            }
-            if(!empty($completed_date)){
-                $completed_date1 = new DateTime($completed_date);
-                $numberDays_completed = $completed_date1->diff($processing_date1);
-                $numberDays_completed1 =$numberDays_completed->format('%a Day and %h hours and %i minutes');
-            }
-            else{
-                $numberDays_completed1="";
-            }
-
-            $data[] = array(
-                'id'          => $order->get_id(),
-                'processing_date'       => $processing_date,
-                'dispatch_date' => $dispatch_date,
-                'completed_date'        => $completed_date,
-                'processing_dispatch'    => $numberDays_dispatch1,
-                'processing_completed'      => $numberDays_completed1
-            );
-
-
-        }
-
-
-
-
-
-        return $data;
-    }
-
-    /**
-     * Define what data to show on each column of the table
-     *
-     * @param  Array $item        Data
-     * @param  String $column_name - Current column name
-     *
-     * @return Mixed
-     */
-    public function column_default( $item, $column_name )
-    {
-        switch( $column_name ) {
-            case 'id':
-            case 'processing_date':
-            case 'dispatch_date':
-            case 'completed_date':
-            case 'processing_dispatch':
-            case 'processing_completed':
-                return $item[ $column_name ];
-
-            default:
-                return print_r( $item, true ) ;
-        }
-    }
-
-    /**
-     * Allows you to sort the data by the variables set in the $_GET
-     *
-     * @return Mixed
-     */
-    private function sort_data( $a, $b )
-    {
-        // Set defaults
-        $orderby = 'processing_date';
-        $order = 'asc';
-
-        // If orderby is set, use this as the sort column
-        if(!empty($_GET['orderby']))
-        {
-            $orderby = $_GET['orderby'];
-        }
-
-        // If order is set use this as the order
-        if(!empty($_GET['order']))
-        {
-            $order = $_GET['order'];
-        }
-
-
-        $result = strcmp( $a[$orderby], $b[$orderby] );
-
-        if($order === 'asc')
-        {
-            return $result;
-        }
-
-        return -$result;
-    }
-}
